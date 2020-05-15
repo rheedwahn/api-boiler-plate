94f5104f6a46765d1878b868aa66d5c1b609d904
rheedwahn <ridwan2bus@gmail.com>
2020-05-15 12:33:33 +0100

    Check if the frontend base url is present This checks if the front end base url is present in the enviroment variable before allowing it to assume the confirmation mail

--- app/Listeners/SendNewUserEmail.php
+++ app/Listeners/SendNewUserEmail.php
@@ -29,7 +29,12 @@ class SendNewUserEmail
     {
         //send email to user
         $front_end_base = \config('app.fronend_url');
-        $confirmation_link = $front_end_base.'/auth?token='.$event->user->remember_token;
+        if($front_end_base) {
+            $confirmation_link = $front_end_base.'/auth?token='.$event->user->remember_token;
+        }else {
+            $confirmation_link = route('email.verify', ['token' => $event->user->remember_token]);
+        }
+        
         $event->user->notify(new NewUserNotification($event->user, $confirmation_link));
     }
 }
